a6acf9d06f96fc23687af7cc6298b855ba4747a6
kstseng <ddd7975@gmail.com>
2015-04-19 21:07:59 +0800

    make it better

--- mysite/wsgi.py
+++
@@ -1,8 +0,0 @@
-## mysite/wsgi.py
-
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-application = Cling(get_wsgi_application())
