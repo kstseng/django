10995cc3d09029977af0c1ed7975b8645cc7ea56
kstseng <ddd7975@gmail.com>
2015-04-19 10:55:11 +0800

    django

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\David\Dropbox\git-repository\django\myvenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
